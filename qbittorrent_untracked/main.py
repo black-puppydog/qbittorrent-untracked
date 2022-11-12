7bac62790b7fc32963d342b2116348c3210a46a9
Daan Wynen <black.puppydog@gmx.de>
2022-11-13 00:50:45 +0100

    allow excluding folders
    for things we don't mind to keep untracked

--- qbittorrent_untracked/main.py
+++ qbittorrent_untracked/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3.9
+
 import argparse
 import os
 import sys
@@ -13,7 +15,11 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--username", default="admin")
     parser.add_argument("--password", default=None)
     parser.add_argument("--torrent-root-local", default=None)
-    parser.add_argument("torrent_root_server")
+    parser.add_argument("-x", "--exclude", action="append", type=Path,
+                        help=("Files and folders to ignore when finding untracked "
+                              "files. These can be absolute, or relative to the local "
+                              "torrent root."))
+    parser.add_argument("torrent_root_server", type=Path)
     args = parser.parse_args()
     if args.torrent_root_local is None:
         args.torrent_root_local = args.torrent_root_server
@@ -24,10 +30,13 @@ if __name__ == "__main__":
         if args.password is None:
             print("Defaulting to 'adminadmin'", file=sys.stderr)
             args.password = "adminadmin"
+    args.torrent_root_local = Path(args.torrent_root_local)
+    args.exclude = [
+        e if e.is_absolute() else args.torrent_root_local / e
+        for e in args.exclude
+    ]
 print(file=sys.stderr)
 
-torrent_root_server = Path(args.torrent_root_server)
-torrent_root_local = Path(args.torrent_root_local)
 
 # instantiate a Client using the appropriate WebUI configuration
 import qbittorrentapi
@@ -64,7 +73,7 @@ for torrent_info in all_info:
         #     print(pformat(dict(torrent_info)), file=sys.stderr)
         #     print(pformat(dict(file_info)), file=sys.stderr)
         file_path = torrent_path / file_info["name"]
-        tracked_files.add(file_path.relative_to(torrent_root_server).as_posix())
+        tracked_files.add(file_path.relative_to(args.torrent_root_server).as_posix())
 print(f"{len(tracked_files):7} files currently tracked.", file=sys.stderr)
 
 # for debugging we dump everything once to analyze in ipython
@@ -72,19 +81,21 @@ print(f"{len(tracked_files):7} files currently tracked.", file=sys.stderr)
 # with open("tracked.json", "w") as f:
 #     json.dump(list(sorted(tracked_files)), f)
 # with open("seen.json", "w") as f:
-#     json.dump(list(sorted(fname.relative_to(torrent_root_local).as_posix() for
-#                    fname in torrent_root_local.glob("**/*") if fname.is_file())), f)
+#     json.dump(list(sorted(fname.relative_to(args.torrent_root_local).as_posix() for
+#                    fname in args.torrent_root_local.glob("**/*") if fname.is_file())), f)
 # sys.exit()
 
 print("Beginning file system scan...", file=sys.stderr)
 print(file=sys.stderr)
 untracked = 0
 total_seen = 0
-for fname in tqdm(torrent_root_local.glob("**/*")):
+for fname in tqdm(args.torrent_root_local.glob("**/*")):
+    if any(fname.is_relative_to(e) for e in args.exclude):
+        continue
     if not fname.is_file():
         continue
     total_seen += 1
-    relative = fname.relative_to(torrent_root_local).as_posix()
+    relative = fname.relative_to(args.torrent_root_local).as_posix()
     if relative not in tracked_files:
         tqdm.write(relative)
         untracked += 1
